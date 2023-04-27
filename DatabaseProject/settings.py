172b5de6fe0dd067f4a25e3567e8d06faa4e00da
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2023-04-27 20:42:48 +0530

    Dummy database data

--- DatabaseProject/settings.py
+++ DatabaseProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['.vercel.app', '*']
 # Application definition
 
 INSTALLED_APPS = [
+    'dbase.apps.DbaseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
