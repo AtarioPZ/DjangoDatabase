b73220f989ca5295707ae7d24072e0a61c71d6c8
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2023-04-27 11:58:55 +0530

    start

--- dbase/urls.py
+++ dbase/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from dbase import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    
+    path("", views.home, name='Home'),
+    path("Home", views.home, name='home')
 ]
