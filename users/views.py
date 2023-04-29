963425a533b63deb1c9c3ebc9229efd8e2c7ee29
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2023-04-29 19:27:40 +0530

    Login

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Created and submitted successfully for {username}.')
-            return redirect('Home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
